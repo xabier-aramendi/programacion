d54cc5f4ed0c021c8d5775d4e7fd0d758621f6af
Xabier Aramendi <xaramendi@birt.eus>
2025-10-01 15:54:31 +0200

    cambiar de nombre el archivo y algunas correciones

--- UD01.PROG01.TareaEvaluación.Piramide/Torre.java
+++ UD01.PROG01.TareaEvaluación.Piramide/Torre.java
@@ -12,10 +12,10 @@
  * 
  *******************************************************************************************************************/
 
-public class Piramide {
+public class Torre {
     public static void main(String[] args) {
     
-        // patrones
+        // patrones que se repiten a lo largo de la estructura
         String v = "V";
         String teja = "__/";
         String tejaSombra = "\\__";
@@ -43,8 +43,9 @@ public class Piramide {
                 System.out.print(ladrillo);
             }
            System.out.print(pilar);
+ 
            for (int j = 0; j < i - 1; j++) {
-                System.out.print(":::");
+                System.out.print(ladrillo);
             }
 
             System.out.println("\\__"); 
@@ -54,14 +55,16 @@ public class Piramide {
         System.out.println("|========================|");
         
         //filas 10 al 13
+        //Aunque en el enunciado aparecen 25 "V"s, he sustituido el valor por 26, para dar con una regla que se repita entre las lineas 10 y 13 -> Cada fila tiene cuatro v-s menos que la anterior fila.
         int ancho = 26;
         
-        for (int i = 0; i <= 4; i++) {
+        for (int i = 0; i < 4; i++) {
           
           for (int j = 0; j < i * 2; j++) { 
             System.out.print(" ");
           }   
           
+          //Se reduce en cuatro "V"s la longitud de cada linea. 
           for (int j = 0; j < ancho - (i * 4); j++) {
              System.out.print("V");
           }
@@ -71,7 +74,7 @@ public class Piramide {
           
         
         //filas 14 al 25
-        for (int j = 1; j <= 13; j++) {
+        for (int j = 1; j <= 12; j++) {
             for (int i = 1; i < 5; i++) {
                 System.out.print("   ");
             }
@@ -97,12 +100,7 @@ public class Piramide {
         }
         
         //fila 29
-        System.out.println("|========================|"); 
-
-        
-        
-        
-        
+        System.out.println("|========================|");        
         
     }
 }
