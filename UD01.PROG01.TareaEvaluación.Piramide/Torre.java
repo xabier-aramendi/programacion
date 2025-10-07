239d5da1dc463ce2d1d298835ee449b26b901078
Xabier Aramendi <xaramendi@birt.eus>
2025-10-07 08:00:55 +0200

    Organizar por métodos para no repetir código

--- UD01.PROG01.TareaEvaluación.Piramide/Torre.java
+++ UD01.PROG01.TareaEvaluación.Piramide/Torre.java
@@ -13,94 +13,85 @@
  *******************************************************************************************************************/
 
 public class Torre {
-    public static void main(String[] args) {
     
-        // patrones que se repiten a lo largo de la estructura
-        String v = "V";
-        String teja = "__/";
-        String tejaSombra = "\\__";
-        String ladrillo = ":::";
-        String pilar = "||";
+   static String v = "V";
+   static String teja = "__/";
+   static String tejaSombra = "\\__";
+   static String ladrillo = ":::";
+   static String pilar = "||";
+   static int filasTejado = 4;
+   
+              
+        public static void main(String[] args) {
         
-        int filasTejado = 4;
-        
-        // filas 1 al 4
-        for (int j = 1; j <= 4; j++) {
-            for (int i = 1; i < 5; i++) {
-                System.out.print("   ");
-            }
-        System.out.print(pilar); 
-        System.out.println(); 
+         imprimirColumnas();     
+         imprimirTejado();
+         imprimirVentanal();
+         imprimirSuelo();
+         imprimirColumnas();
+         imprimirTejado();
+         imprimirVentanal();
+         
         }
         
-        // filas 4 al 8
-        for (int i = 1; i <= 4; i++) {
-            for ( int j = i; j < filasTejado; j++) {  
-               System.out.print("   ");
+        // filas 1 al 4
+        public static void imprimirColumnas() {
+           for (int j = 1; j <= 4; j++) {
+               for (int i = 1; i < 5; i++) {
+                   System.out.print("   ");
                }
-            System.out.print("__/");
-            for ( int j = 0; j < i - 1; j++) { 
-                System.out.print(ladrillo);
-            }
-           System.out.print(pilar);
- 
-           for (int j = 0; j < i - 1; j++) {
-                System.out.print(ladrillo);
-            }
-
-            System.out.println("\\__"); 
-        } 
+           System.out.print(pilar); 
+           System.out.println(); 
+           }  
+        }   
+        
+        // filas 4 al 8
+        public static void imprimirTejado() {
+           for (int i = 1; i <= 4; i++) {
+               for ( int j = i; j < filasTejado; j++) {  
+                  System.out.print("   ");
+                  }
+               System.out.print("__/");
+               for ( int j = 0; j < i - 1; j++) { 
+                   System.out.print(ladrillo);
+               }
+              System.out.print(pilar);
+           
+              for (int j = 0; j < i - 1; j++) {
+                   System.out.print(ladrillo);
+               }
+   
+               System.out.println("\\__"); 
+           } 
+        }
         
         //fila 9
-        System.out.println("|========================|");
+        public static void imprimirVentanal() {
+            System.out.println("|========================|");
+        }
+        
         
         //filas 10 al 13
         //Aunque en el enunciado aparecen 25 "V"s, he sustituido el valor por 26, para dar con una regla que se repita entre las lineas 10 y 13 -> Cada fila tiene cuatro v-s menos que la anterior fila.
-        int ancho = 26;
-        
-        for (int i = 0; i < 4; i++) {
-          
-          for (int j = 0; j < i * 2; j++) { 
-            System.out.print(" ");
-          }   
-          
-          //Se reduce en cuatro "V"s la longitud de cada linea. 
-          for (int j = 0; j < ancho - (i * 4); j++) {
-             System.out.print("V");
-          }
-        
-        System.out.println();
-        }
-          
-        
-        //filas 14 al 25
-        for (int j = 1; j <= 12; j++) {
-            for (int i = 1; i < 5; i++) {
-                System.out.print("   ");
-            }
-        System.out.print(pilar); 
-        System.out.println(); 
+        public static void imprimirSuelo() {
+           
+           int ancho = 26;
+           
+           for (int i = 0; i < 4; i++) {
+             
+             for (int j = 0; j < i * 2; j++) { 
+               System.out.print(" ");
+             }   
+             
+             //Se reduce en cuatro "V"s la longitud de cada linea. 
+             for (int j = 0; j < ancho - (i * 4); j++) {
+                System.out.print("V");
+             }
+           
+           System.out.println();
+           }        
         }
         
-        // filas 25 al 28
-        for (int i = 1; i <= 4; i++) {
-            for ( int j = i; j < filasTejado; j++) {  
-               System.out.print("   ");
-            }
-            System.out.print("__/");
-            for ( int j = 0; j < i - 1; j++) { 
-                System.out.print(ladrillo);
-            }
-           System.out.print(pilar);
-           for (int j = 0; j < i - 1; j++) {
-                System.out.print(":::");
-            }
-
-            System.out.println("\\__"); 
-        }
         
-        //fila 29
-        System.out.println("|========================|");        
-        
-    }
+      
 }
